1ec97e4eaec732530a4ec47a6b468e1aa38ce2ad
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-03-26 21:08:08 +0530

    Added tags.go

---
+++ tags.go
@@ -0,0 +1,12 @@
+package gomatrix
+
+// Tags is based on the Room Tagging feature as described in https://matrix.org/docs/spec/client_server/r0.2.0.html#room-tagging
+// Tag contains the data for a Tag which can be referenced by the Tag name
+type Tag struct {
+	Tags map[string]TagProperties `json:"tags"`
+}
+
+// TagProperties contains the properties of an MTag
+type TagProperties struct {
+	Order float32 `json:"order,omitempty"` // Empty values must be neglected
+}
