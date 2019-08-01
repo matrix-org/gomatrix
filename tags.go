35ae5fadeb02fe2b33a890772d565ed6e53e36a3
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-08-01 15:48:58 +0530

    Minor change

--- tags.go
+++ tags.go
@@ -17,7 +17,7 @@ package gomatrix
 // TagContent contains the data for an m.tag message type
 // https://matrix.org/docs/spec/client_server/r0.4.0.html#m-tag
 type TagContent struct {
-	Tags map[string]TagProperties `json:"tags,omitemmpty"`
+	Tags map[string]TagProperties `json:"tags,omitempty"`
 }
 
 // TagProperties contains the properties of a Tag
