64c39d4e4d7d5eebf6b35516c2441f2d9a49ca5f
Bernhard Tittelbach <bernhard@tittelbach.org>
2019-08-30 20:59:34 +0200

    support ephemeral events

--- responses.go
+++ responses.go
@@ -159,6 +159,9 @@ type RespSync struct {
 				Limited   bool    `json:"limited"`
 				PrevBatch string  `json:"prev_batch"`
 			} `json:"timeline"`
+			Ephemeral struct {
+				Events []Event `json:"events"`
+			} `json:"ephemeral"`
 		} `json:"join"`
 		Invite map[string]struct {
 			State struct {
